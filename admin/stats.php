a6f89076b0c9d212670b2ce9a0c221678b33a384
elliotcondon <elliotcondon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-06 04:16:33 +0000

    m
    git-svn-id: https://plugins.svn.wordpress.org/wordpress3-invoice/tags/1.0@260656 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/stats.php
@@ -0,0 +1,441 @@
+<?php
+/*--------------------------------------------------------------------------------------------
+									Global Variables
+--------------------------------------------------------------------------------------------*/
+$firstInvoiceYear = '';
+$lastInvoiceYear = '';
+
+$financialYear = true;
+
+$financialYearStart = '';
+$financialYearEnd = '';
+
+$filterMonth = '07';
+$filterYearStart = '';
+$filterYearEnd = '';
+
+$invoices;
+
+$graphTitles = array();
+$graphDates = array();
+$graphTotals = array();
+
+$graphTitlesOutstanding = array();
+$graphTotalsOutstanding = array();
+
+
+/*--------------------------------------------------------------------------------------------
+									wp3i_setup_variables
+									
+* This function gets all the global variables and assignes the default values
+* It then works out $_POST data and overrides the defaults.
+--------------------------------------------------------------------------------------------*/
+function wp3i_setup_variables()
+{
+	global $financialYearStart, $financialYearEnd, $financialYear;
+	global $firstInvoiceYear, $lastInvoiceYear;
+	global $filterYearStart, $filterYearEnd, $filterMonth;
+	
+	
+	
+	/* Setup Invoice Years (First - Last)
+	--------------------------------------*/
+	$firstInvoiceYear = get_posts(array('post_type' => 'invoice', 'numberposts' => '1', 'orderby' => 'date', 'order' => 'ASC' )); 
+	$firstInvoiceYear = $firstInvoiceYear[0]->post_date; $firstInvoiceYear = substr($firstInvoiceYear,0,4);
+	
+	$lastInvoiceYear = get_posts(array('post_type' => 'invoice', 'numberposts' => '1', 'orderby' => 'date', 'order' => 'DESC' )); 
+	$lastInvoiceYear = $lastInvoiceYear[0]->post_date; $lastInvoiceYear = substr($lastInvoiceYear,0,4);
+	
+	if($lastInvoiceYear == $firstInvoiceYear){$lastInvoiceYear = $firstInvoiceYear + 1;}
+	
+	
+	
+	/* Setup Financial Year (Start - End)
+	--------------------------------------*/
+	
+	if(date('m') >= 7)
+	{
+		$financialYearStart = date('Y');	
+		$financialYearEnd = date('Y') + 1;
+	}
+	else
+	{
+		$financialYearStart = date('Y') - 1;	
+		$financialYearEnd = date('Y');
+	}
+	
+	
+	
+	/* Determin Filter Start - End
+	--------------------------------------*/
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		$financialYear = isset($_POST['financial-year'])? true: false;
+		$filterMonth = isset($_POST['financial-year'])? '07': '01';
+		$filterYearStart = $_POST['filter-year'];
+		$filterYearEnd = $_POST['filter-year'] + 1;
+	}
+	else
+	{
+		$financialYear = true;
+		$filterMonth = '07';
+		$filterYearStart = $financialYearStart;
+		$filterYearEnd = $financialYearStart + 1;
+	}
+		
+}
+
+
+/*--------------------------------------------------------------------------------------------
+									wp3i_filter_invoices
+										
+* This function will use the filter variables (yearStart, month, yearEnd)
+* It filters the invoices returned when using get_posts
+--------------------------------------------------------------------------------------------*/
+function wp3i_filter_invoices()
+{
+	function financialYearFilter($where = '') 
+	{
+		global $filterYearStart, $filterMonth, $filterYearEnd;
+		$where .= " AND post_date >= '".$filterYearStart."-".$filterMonth."-01' AND post_date <= '".$filterYearEnd."-".$filterMonth."-01'";
+		return $where;
+	}
+	add_filter('posts_where','financialYearFilter');
+}
+
+
+
+
+/*--------------------------------------------------------------------------------------------
+									wp3i_get_last_financial_year
+										
+* If the current date is past June 30, this function will return the current year.
+* If the current date is befor June 30, this function will return last year.
+--------------------------------------------------------------------------------------------*/
+function wp3i_get_last_financial_year()
+{
+	if(date('m') >= 7)
+	{
+		return date('Y');	
+	}
+	else
+	{
+		return date('Y') - 1;
+	}
+}
+
+
+
+
+/*--------------------------------------------------------------------------------------------
+									wp3i_setup_graph_data
+										
+* Loops though all available invoices and sets data for:
+* Titles, Dates and Totals
+--------------------------------------------------------------------------------------------*/
+function wp3i_setup_graph_data()
+{
+	global $invoices, $graphTitles, $graphTitlesOutstanding, $graphDates, $graphTotals, $graphTotalsOutstanding;
+	global $financialYear;
+	
+	$invoicesReversed = array_reverse($invoices);
+	$total = 0.00;
+	
+	// create blank/default graph arrays
+	$graphDates = array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+	$graphTitles = array('', '', '', '', '', '', '', '', '', '', '', '');
+	$graphTotals = array(0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00);
+	$graphTitlesOutstanding = array('', '', '', '', '', '', '', '', '', '', '', '');
+	$graphTotalsOutstanding = array(0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00);
+	
+	// add invoice data into array
+	foreach($invoicesReversed as $invoice)
+	{
+		$invoiceStatusCF = get_post_meta($invoice->ID, 'invoice_status', true);
+		if($invoiceStatusCF == 'Invoice Paid')
+		{
+			$invoiceMonth = get_the_time('n',$invoice->ID);
+			
+			$graphTitles[$invoiceMonth-1] .= get_post_meta($invoice->ID, 'invoice_number', true) . ' - ' . $invoice->post_title . '<br>';
+			$graphTotals[$invoiceMonth-1] += wp3i_get_invoice_total($invoice->ID);
+		}
+		elseif($invoiceStatusCF == 'Invoice Sent')
+		{
+			$invoiceMonth = get_the_time('n',$invoice->ID);
+			$graphTitlesOutstanding[$invoiceMonth-1] .= get_post_meta($invoice->ID, 'invoice_number', true) . ' - ' . $invoice->post_title . '<br>';
+			$graphTotalsOutstanding[$invoiceMonth-1] += wp3i_get_invoice_total($invoice->ID);
+		}
+	}
+	
+	// if financial year is selected, arange the arrays so they start from Jun
+	if($financialYear == true)
+	{
+		$graphDates = array_merge(array_slice($graphDates,6,6), array_slice($graphDates,0,6));
+		$graphTitles = array_merge(array_slice($graphTitles,6,6), array_slice($graphTitles,0,6));
+		$graphTotals = array_merge(array_slice($graphTotals,6,6), array_slice($graphTotals,0,6));
+		
+		$graphTitlesOutstanding = array_merge(array_slice($graphTitlesOutstanding,6,6), array_slice($graphTitlesOutstanding,0,6));
+		$graphTotalsOutstanding = array_merge(array_slice($graphTotalsOutstanding,6,6), array_slice($graphTotalsOutstanding,0,6));
+	}
+}
+
+
+
+
+/*--------------------------------------------------------------------------------------------
+									wp3i_stats_graph
+										
+* Creates graph of income per month
+--------------------------------------------------------------------------------------------*/
+function wp3i_stats_graph()
+{
+	global $firstInvoiceYear, $graphTitles, $graphTitlesOutstanding, $graphDates, $graphTotals, $graphTotalsOutstanding;
+	global $filterMonthStart, $filterYearStart, $filterMonthEnd, $filterYearEnd;
+	
+	global $financialYear;
+	
+	
+	/* Work out Graph Title
+	------------------------*/
+	$graphTitle = '';
+	if($financialYear == true){$graphTitle .= 'Financial Year ';}else{$graphTitle .= 'Normal Year ';}
+	$graphTitle .= $filterYearStart.' - '.$filterYearEnd;
+	
+	wp3i_setup_graph_data();
+	?>
+    
+    <script type="text/javascript">
+	var chart;
+	var $ = jQuery.noConflict();
+		
+	$(document).ready(function() {
+		chart = new Highcharts.Chart({
+			chart: {
+				renderTo: 'chart',
+				defaultSeriesType: 'line',
+				margin: [50, 0, 60, 60]
+			},
+			title: {
+				text: '<?php echo $graphTitle;?>',
+			},
+			xAxis: {
+				//categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+
+				categories: [<?php foreach($graphDates as $graphDate){echo "'".$graphDate."', ";} ?>],
+					title: {
+						text: 'Month'
+					},
+					labels: {
+						formatter: function() {
+							return this.value;
+						}
+					},
+
+				},
+				yAxis: {
+					title: {
+						text: 'Income'
+					},
+					plotLines: [{
+						value: 0,
+						width: 1,
+						color: '#808080'
+					}]
+				},
+				tooltip: {
+					formatter: function() {
+			                return '<b>'+ this.point.name +'</b><br/>'+
+							'Total '+this.series.name+' '+this.x + ': $'+this.y;
+					}
+				},
+				legend: {
+					layout: 'vertical',
+					style: {
+						left: 'auto',
+						bottom: 'auto',
+						right: '0px',
+						top: '0px'
+					}
+				},
+				colors: [
+					'#5fb300', 
+					'#e9bf00'
+				],
+				series: [{
+					name: 'Income',
+					data: [
+					<?php
+					for($i = 0; $i < count($graphDates); $i++)
+					{
+						echo '{';
+						echo "name: '".$graphTitles[$i]."',";
+						//echo "x: '".$graphDates[$i]."', ";
+						echo "y: ".$graphTotals[$i]."";
+						echo '}, ';
+					}
+					?>
+					]
+				},{
+					name: 'Outstanding',
+					data: [
+					<?php
+					for($i = 0; $i < count($graphDates); $i++)
+					{
+						echo '{';
+						echo "name: '".$graphTitlesOutstanding[$i]."',";
+						//echo "x: '".$graphDates[$i]."', ";
+						echo "y: ".$graphTotalsOutstanding[$i]."";
+						echo '}, ';
+					}
+					?>
+					]
+				}]
+			});
+			
+			
+		});
+		</script>
+	<div class="wp3i-stats-graph" id="chart"></div>
+    <?php
+}
+
+function wp3i_get_invoice_total($invoiceID)
+{
+	$total = 0.00;
+	$detailSubtotal = unserialize(get_post_meta($invoiceID, 'detail_subtotal', true));
+	foreach($detailSubtotal as $subtotal)
+	{
+		$total += floatval($subtotal);
+	}
+	return $total;
+}
+
+function wp3i_stats_total($invoiceStatus)
+{
+	global $invoices;
+	$total = 0.00;
+	foreach($invoices as $invoice)
+	{
+		$invoiceStatusCF = get_post_meta($invoice->ID, 'invoice_status', true);
+		if($invoiceStatusCF == $invoiceStatus)
+		{
+			$total += wp3i_get_invoice_total($invoice->ID);
+		}
+	}
+	echo number_format($total, 2, '.', '');
+}
+
+function wp3i_stats_invoices()
+{
+	global $invoices;
+	$counter = 0;
+	echo '<table><tr><th>#</th><th>Invoice Name</th><th>Date</th><th>Total</th><th></th></tr>';
+	foreach($invoices as $invoice)
+	{
+		$counter++; ?>
+		<tr <?php if($counter % 2 == 0){echo 'class="alternate"';} ?>>
+            <td class="invoice-number"><?php echo get_post_meta($invoice->ID, 'invoice_number', true); ?></td>
+            <td><?php edit_post_link($invoice->post_title,'','',$invoice->ID); ?></td>
+            <td class="invoice-date"><?php echo get_the_time('d M Y',$invoice->ID); ?></td>
+            <td class="total"><?php echo number_format(wp3i_get_invoice_total($invoice->ID), 2, '.', ''); ?></td>
+            <td><?php $invoiceStatusCF = get_post_meta($invoice->ID, 'invoice_status', true); 
+			if($invoiceStatusCF == 'Invoice Paid')
+			{
+				echo'<div class="tick"></div>';
+			}
+			else
+			{
+				echo'<div class="cross"></div>';
+			} ?></td>
+        </tr>
+		<?php 
+	}
+	echo '</table>';
+}
+
+
+/*--------------------------------------------------------------------------------------------
+											Stats Page
+--------------------------------------------------------------------------------------------*/
+function wp3i_stats()
+{
+	/* Global Variable
+	--------------------------------------*/
+	global $invoices, $financialYearStart, $financialYear;
+	global $filterYearStart, $filterYearEnd, $filterMonth;
+	
+	wp3i_setup_variables();
+	wp3i_filter_invoices();
+	
+	
+	/* Get Filtered Invoices
+	--------------------------------------*/
+	$invoices = get_posts(array(
+		'post_type' => 'invoice', 
+		'numberposts' => '-1', 
+		'suppress_filters' => false 
+	));
+	
+	?>
+    
+    
+    <div class="wrap">
+    	<!--<div class="icon32" id="icon-wp3i"><br></div>-->
+    	<h2>WP3 Invoice Stats</h2>
+        <form method="post" action="" id="wp3i-stats">
+			<div id="poststuff">
+            
+                <ul class="subsubsub">
+                    <li>Display from </li>
+                    <li><select name="filter-year" id="filter-year">
+                    	<?php global $firstInvoiceYear, $lastInvoiceYear; ?>
+                    	<?php for($i= $lastInvoiceYear; $i >= $firstInvoiceYear; $i--): ?>
+                    		<option value="<?php echo $i; ?>" <?php if($i == $filterYearStart){echo 'selected="selected"';} ?>><?php echo $i; ?> - <?php echo $i+1; ?></option>
+                        <?php endfor; ?>
+                    </select></li>
+                    <li>
+                    	<input type="checkbox" name="financial-year" id="financial-year" value="yes" <?php if($financialYear == true){echo 'checked="checked"';} ?> /> Financial Year
+                    </li>
+                    <li><input type="submit" class="button-secondary" value="Filter" id="post-query-submit"></li>
+                </ul>
+    
+                
+                <div class="postbox" id="wp3i-stats-graph">
+                    <h3 class="hndle"><span>WP3 Invoice Stats</span></h3>
+                    <div class="inside">
+                        
+                        <div style="overflow:hidden;">
+							<?php wp3i_stats_graph(); ?>
+                            
+                            <div class="summary">
+                                <div id="income" class="postbox">
+                                    <h3 class="hndle"><span>Income</span></h3>
+                                    <div class="inside">
+                                        <h2>$<?php wp3i_stats_total('Invoice Paid'); ?></h2>
+                                    </div>
+                                </div>
+                                <div id="outstanding" class="postbox">
+                                    <h3 class="hndle"><span>Outstanding</span></h3>
+                                    <div class="inside">
+                                        <h2>$<?php wp3i_stats_total('Invoice Sent'); ?></h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="wp3i-stats-invoices">
+                        	<?php wp3i_stats_invoices(); ?>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+		</form>
+    </div>
+<?php
+}
+
+
+
+
+?>
---
+++ wp3-invoice.php
@@ -0,0 +1,176 @@
+<?php
+/*
+Plugin Name: WordPress 3 Invoice
+Plugin URI: http://www.elliotcondon.com/
+Description: An online Invoice solution for web designers. Manage invoices through wordpress and customise with html + css invoice templates.
+Version: 1.0
+Author: Elliot Condon
+Author URI: http://www.elliotcondon.com/
+License: GPL
+*/
+
+
+$invoice_template_url = plugins_url('template',__FILE__);
+
+
+/*--------------------------------------------------------------------------------------------
+											Head
+--------------------------------------------------------------------------------------------*/
+function wp3i_head()
+{
+	?>
+	<link rel="stylesheet" href="<?php echo plugins_url('admin/style.css',__FILE__); ?>" type="text/css" media="all" />	
+    <script type="text/javascript" src="<?php echo plugins_url('admin/admin-jquery.js',__FILE__); ?>" ></script>
+    <script type="text/javascript" src="<?php echo plugins_url('admin/highcharts.js',__FILE__); ?>"></script>
+    <?php
+}
+add_action('admin_head', 'wp3i_head');
+
+
+/*--------------------------------------------------------------------------------------------
+										WP3I Init
+--------------------------------------------------------------------------------------------*/
+function wp3i_init()
+{
+	
+	/* Regist Invoice Post Type
+	----------------------------*/
+	$labels = array(
+		'name' => __( 'Invoices' ),
+		'singular_name' => __( 'Invoice'),
+		'search_items' =>  __( 'Search Invoices' ),
+		'all_items' => __( 'All Invoices' ),
+		'parent_item' => __( 'Parent Invoice' ),
+		'parent_item_colon' => __( 'Parent Invoice:' ),
+		'edit_item' => __( 'Edit Invoice' ), 
+		'update_item' => __( 'Update Invoice' ),
+		'add_new_item' => __( 'Add New Invoice' ),
+		'new_item_name' => __( 'New Invoice Name' ),
+	); 	
+	register_post_type('invoice', array(
+		'labels' => $labels,
+		'menu_icon' => plugins_url('admin/images/invoice-icon.gif',__FILE__),
+		'public' => true,
+		'show_ui' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'rewrite' => array("slug" => "invoice"), // Permalinks format
+		'supports' => array('title'),
+	));
+	
+	
+	/* Clients Taxonomy
+	----------------------------*/
+	$labels = array(
+		'name' => _x( 'Clients', 'taxonomy general name' ),
+		'singular_name' => _x( 'Client', 'taxonomy singular name' ),
+		'search_items' =>  __( 'Search Clients' ),
+		'all_items' => __( 'All Clients' ),
+		'parent_item' => __( 'Parent Client' ),
+		'parent_item_colon' => __( 'Parent Client:' ),
+		'edit_item' => __( 'Edit Client' ), 
+		'update_item' => __( 'Update Client' ),
+		'add_new_item' => __( 'Add New Client' ),
+		'new_item_name' => __( 'New Client Name' ),
+	); 	
+	register_taxonomy('client', 'invoice',
+		array(
+             'hierarchical' => true,
+			 'labels' => $labels,
+			 'query_var' => true,
+			 'rewrite' => true
+		)
+	);
+
+
+	/* Invoice Columns
+	----------------------------*/
+	function wp3i_columns($columns)
+	{
+		$columns = array(
+			"cb" => "<input type=\"checkbox\" />",
+			"invoice_no" => "Invoice No.",
+			"title" => "Title",
+			"amount" => "Amount",
+			"status" => "Status",
+			"client" => "Client",
+		);
+		return $columns;
+	}
+	
+	function wp3i_date_diff($start, $end) 
+	{
+		$start_ts = strtotime($start);
+		$end_ts = strtotime($end);
+		$diff = $end_ts - $start_ts;
+		return round($diff / 86400);
+	}
+
+	function my_custom_columns($column)
+	{
+		global $post;
+		if ("ID" == $column) echo $post->ID;
+		elseif ("description" == $column) echo $post->post_content;
+		elseif ("invoice_no" == $column) echo get_post_meta($post->ID, 'invoice_number', true);
+		elseif ("amount" == $column) echo '$'.number_format(wp3i_get_invoice_total($post->ID), 2, '.', '');
+		elseif ("client" == $column) echo get_the_term_list( $post->ID, 'client', '', ', ', '' );  
+		elseif ("status" == $column)
+		{
+			if(get_post_meta($post->ID, 'invoice_status', true) == 'Invoice Paid'){echo 'Paid';}
+			elseif(get_post_meta($post->ID, 'invoice_status', true) == 'Invoice Sent')
+			{
+				$days = wp3i_date_diff(get_the_time('Y-m-d', $post->ID), date('Y-m-d'));
+				if($days == 0){echo 'Sent today';}
+				elseif($days == 1){echo 'Sent 1 day ago';}
+				else{ echo 'Sent '.$days.' days ago';} 
+			}
+			else{echo get_post_meta($post->ID, 'invoice_status', true);}
+		}
+	}
+	
+	add_action("manage_posts_custom_column", "my_custom_columns");
+	add_filter("manage_edit-invoice_columns", "wp3i_columns");
+	
+}
+add_action('init', 'wp3i_init');
+
+
+
+	/* Template Redirect
+	----------------------------*/
+	function wp3i_template_redirect()
+	{
+		global $post;
+		$invoiceStatus = get_post_meta($post->ID, 'invoice_status', true);
+		
+		if(get_post_type($post) == 'invoice' && $invoiceStatus != 'Quote')
+		{
+			require_once('template/invoice.php');
+			die();
+		}
+		if(get_post_type($post) == 'invoice' && $invoiceStatus == 'Quote')
+		{
+			require_once('template/quote.php');
+			die();
+		}
+	}
+	add_action('template_redirect', 'wp3i_template_redirect');
+
+
+	
+	/* Includes
+	----------------------------*/
+	require_once('admin/meta-boxes.php');
+	require_once('admin/stats.php');
+	require_once('core/functions.php');
+	
+	
+function wp3i_menu()
+{
+	add_submenu_page('edit.php?post_type=invoice', 'WP3 Invoice Stats', 'Stats', 'manage_options', 'wp3-invoice-stats', 'wp3i_stats');
+}
+add_action('admin_menu', 'wp3i_menu');
+
+
+
+
